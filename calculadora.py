eadf407b3a4fc272bdf9d908c32ef894ed52239c
pedrolisboa07 <pedrolal1406@gmail.com>
2026-09-21 19:27:28 -0300

    Refactor comments in calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+def somar(a, b):
+    #Retorna a soma 
+    return a + b
+
+def subtrair(a, b):
+    #Retorna a diferença 
+    return a - b
+
+def multiplicar(a, b):
+    #Retorna o produto
+    return a * b
+
+def dividir(a, b):
+    #Retorna a divisão 
+    if b == 0:
+        return None  # Ou "Erro: "
+    return a / b
